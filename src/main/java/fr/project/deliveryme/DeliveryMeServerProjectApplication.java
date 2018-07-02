5e9c862394646a17dd3ed5028b106beb4d149062
Ahmed <achaouch@WEBNET.LOCAL>
2018-07-02 13:57:35 +0200

    Commit 1 - Server Side Project Setting Up

---
+++ src/main/java/fr/project/deliveryme/DeliveryMeServerProjectApplication.java
@@ -0,0 +1,12 @@
+package fr.project.deliveryme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMeServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMeServerProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/project/deliveryme/DeliveryMeServerProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.project.deliveryme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeliveryMeServerProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
